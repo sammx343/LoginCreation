aafbca463f00f05c33c3c27212e74562871cff5a
Sebastian Mayor Molinares <smayor@redsis.com>
2018-06-07 12:32:37 -0500

    session ended

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Importar Rutas
+import { APP_ROUTING } from './app.routes';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+
+import { AuthGuardService } from './services/auth-guard.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PreciosComponent,
+    ProtegidaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    AuthGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+(window as any).global = window;
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styles: []
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private auth:AuthService) { 
+    auth.handleAuthentication()
+  }
+
+  ngOnInit() {
+  }
+
+  login(){
+    console.log("hola");
+    this.auth.login();
+  }
+
+  salir(){
+    this.auth.logout();
+  }
+
+}
